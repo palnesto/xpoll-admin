5642a3d2fa407b6f83200028c9e219ffd631b09b
saranshkhulbe7 <saranshkhulbe7@gmail.com>
2025-10-05 18:20:54 +0530

    llm

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,6 +1,12 @@
 import { UILayoutRenderer } from "@/components/UILayoutRenderer";
 import jsonData from "@/result.json";
+import { useNavigate } from "react-router";
 const TestPage = () => {
+  const navigate = useNavigate();
+  const env = import.meta.env.VITE_MODE;
+  if (env !== "local") {
+    navigate(`/`);
+  }
   return (
     <div>
       <UILayoutRenderer data={jsonData} />
